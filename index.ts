7ca0ffc79cf9079f34d49a6616b552d9702e40f5
sajid365-sr <sajidsorker86@gmail.com>
2023-10-22 04:54:13 +0600

    test prisma with mysql server

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from "@prisma/client";
+
+const Prisma = new PrismaClient();
+
+async function main() {
+  //   const user = await Prisma.user.create({
+  //     data: {
+  //       name: "Sajid Sorker",
+  //       email: "sajidsorker@gmail.com",
+  //     },
+  //   });
+
+  //   const getUser = await Prisma.user.findMany();
+  const updateUer = await Prisma.user.update({
+    where: {
+      id: "31748427-4a50-4f21-baa8-c90f5dc1b659",
+    },
+    data: {
+      name: "Srabonti Akter",
+      email: "akter.srabonti@gmail.com",
+    },
+  });
+
+  console.log(updateUer);
+}
+
+main()
+  .then(async () => {
+    await Prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.log(e);
+    await Prisma.$disconnect();
+    process.exit(1);
+  });
---
+++ prisma/migrations/20231021224444_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Article` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `body` VARCHAR(191) NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Article` ADD CONSTRAINT `Article_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
