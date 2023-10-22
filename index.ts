561128668a5b4116b301e6764ed6f0d9efede904
sajid365-sr <sajidsorker86@gmail.com>
2023-10-22 09:15:42 +0600

    tested different databased

--- index.ts
+++ index.ts
@@ -3,25 +3,26 @@ import { PrismaClient } from "@prisma/client";
 const Prisma = new PrismaClient();
 
 async function main() {
-  //   const user = await Prisma.user.create({
-  //     data: {
-  //       name: "Sajid Sorker",
-  //       email: "sajidsorker@gmail.com",
-  //     },
-  //   });
-
-  //   const getUser = await Prisma.user.findMany();
-  const updateUer = await Prisma.user.update({
-    where: {
-      id: "31748427-4a50-4f21-baa8-c90f5dc1b659",
-    },
+  const createUser = await Prisma.user.create({
     data: {
-      name: "Srabonti Akter",
-      email: "akter.srabonti@gmail.com",
+      name: "Sajid Sorker",
+      email: "contact@sajidsorker.com",
+      phone: 1608331365,
     },
   });
 
-  console.log(updateUer);
+  //   const getUser = await Prisma.user.findMany();
+  //   const updateUer = await Prisma.user.update({
+  //     where: {
+  //       id: "31748427-4a50-4f21-baa8-c90f5dc1b659",
+  //     },
+  //     data: {
+  //       name: "Srabonti Akter",
+  //       email: "akter.srabonti@gmail.com",
+  //     },
+  //   });
+
+  console.log(createUser);
 }
 
 main()
